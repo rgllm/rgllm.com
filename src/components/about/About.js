93d51a2d368ff08217abc18eb8691e2fa4f832b0
Rogério Moreira <r@rgllm.com>
2018-08-12 12:17:56 +0100

    removing lorem

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -14,11 +14,8 @@ class About extends React.Component {
             <main class="site-content">
                     <section class="intro-title screen-height header-compensated center-vertically">
                         <div class="section-inner fade-block">
-                            <h1 class="big-title graphics-title">About me</h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit orci facilisis dis bibendum sapien magna congue sem vivamus enim nisl suspendisse ullamcorper cras auctor tortor curae praesent integer class purus quisque tristique ut neque luctus litora id nunc sed cum volutpat tincidunt vel ligula pulvinar ultrices sociosqu ornare pharetra porta habitant fames nam mauris ridiculus torquent venenatis nisi conubia iaculis dui habitasse lobortis diam maecenas feugiat leo tempor taciti tempus lectus a at donec eu montes mus egestas lacus ultricies platea malesuada lacinia aptent vitae fringilla commodo non primis eros placerat natoque curabitur senectus sagittis sodales duis interdum nullam est accumsan</p>
-                            <br/>
-                            <br/>
-                            <Link to="/cv">My resume</Link>
+                            <Link to="/cv"><h1 class="big-title graphics-title">About me</h1></Link>
+                            <p>🕶</p>
                         </div>
                     </section>
             </main>
