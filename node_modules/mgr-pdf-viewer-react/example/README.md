8161690142c186d2a282fd84fc7427f1c85f9a23
Rogério Moreira <r@rgllm.com>
2018-08-16 16:11:27 +0100

    header update and npm packages

---
+++ node_modules/mgr-pdf-viewer-react/example/README.md
@@ -0,0 +1,1 @@
+# mgr-pdf-viewer-react example
---
+++ node_modules/react-pdfjs/lib/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
+
+exports.__esModule = true;
+
+var _Pdf = require('./Pdf');
+
+var _Pdf2 = _interopRequireDefault(_Pdf);
+
+exports['default'] = _Pdf2['default'];
+module.exports = exports['default'];
