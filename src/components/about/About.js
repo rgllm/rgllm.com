c3b1bb06252e76f6cff762e39d42aa394b289b42
Rogério Moreira <r@rgllm.com>
2018-08-19 17:46:40 +0100

    added google analytics

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,15 +11,64 @@ class About extends React.Component {
 
     render() {
         return(
-            <main class="site-content">
-                    <section class="intro-title screen-height header-compensated center-vertically">
-                        <div class="section-inner fade-block">
-                            <Link to="/cv"><h1 class="big-title graphics-title">About me</h1></Link>
-                            <p>🕶</p>
+            <main className="site-content">
+                    <section className="intro-title screen-height header-compensated center-vertically">
+                        <div className="section-inner fade-block">
+
+                            <section>
+                                <h2>About</h2>
+                                <p>I am a MSc student at University of Minho. I write about Apple and tech at <a href="https://iclub.pt">iClub</a>.</p>
+                            </section>
+
+                            <section>
+                                <h2>Education</h2>
+                                <dl className="timeline">
+                                    <div className="timeline-item">
+                                        <dt><time>Sep 2017 - present</time></dt>
+                                        <dd>MSc in <a href="http://miei.di.uminho.pt/">Software Engineering</a> (Artificial Intelligence and Software Systems) at the <a href="http://www.uminho.pt/">University of Minho</a></dd>
+                                    </div>
+            
+                                    <div className="timeline-item">
+                                        <dt><time>Sep 2014 - Jul 2017</time></dt>
+                                        <dd>BSc in <a href="http://miei.di.uminho.pt/">Software Engineering</a> at the <a href="http://www.uminho.pt/">University of Minho</a></dd>
+                                    </div>
+            
+                                </dl>
+                            </section>
+
+                            <section>
+                                <h2>Talks and Wordkshops</h2>
+                                <p>I also do some speaking work and workshops about tech stuff.</p>
+                                <ul>
+                                    <li><a href="https://www.facebook.com/events/429278770844403/">WordPress workshop for NEEGIUM</a>. Slides <a href="/wpslides.pdf">here</a>.</li>
+                                </ul>
+                            </section>
+
+                            <section>
+                            <h2>Work</h2>
+                            <dl className="timeline">
+                                <div className="timeline-item">
+                                    <dt><time>Fev 2014 - present</time></dt>
+                                    <dd>
+                                    <h3 className="timeline-title">Host <a href="https://www.meetup.com/Meetup-WordPress-de-Braga/">Meetup WordPress de Braga</a></h3>
+                                        <p>Monthly meetup of the WordPress community in Braga.</p>
+                                    </dd>
+                                </div>
+                            </dl>
+                        </section>
+
+                        <section>
+                            <h2>Hackathons</h2>
+                            <p>I like to build things. Hackathons are the perfect opportunity to build things in 24 or 48 hours while stressed, sleep deprived and lots of caffeine.</p>
+                            <ul>
+                                <li><a href="https://github.com/rgllm/helpum">HelpUM</a> at <a href="https://hacktivate.io/">Hacktivate</a> in <time>Feb 2018</time></li>
+                            </ul>
+                        </section>
+
                         </div>
                     </section>
             </main>
-        );
+        )
     }
 
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,24 +11,24 @@ class Header extends React.Component {
 
     render() {
         return (
-            <div class="home page-template page-template-parts">
-                <header class="site-header section-inner">
-                    <div class="header-left">
-                        <h1 class="site-title">
+            <div className="home page-template page-template-parts">
+                <header className="site-header section-inner">
+                    <div className="header-left">
+                        <h1 className="site-title">
 					        <Link to="/" title="Rogério Moreira">Rogério Moreira</Link>
 				        </h1>
-                        <ul class="menu main-menu">
-                            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item"><Link to="/">Home</Link></li>
-                            <li class="menu-item menu-item-type-custom menu-item-object-custom"><Link to="/about">About me</Link></li>
-                            <li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="https://www.blog.rgllm.com/">Writing</a></li>
+                        <ul className="menu main-menu">
+                            <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item"><Link to="/">Home</Link></li>
+                            <li className="menu-item menu-item-type-custom menu-item-object-custom"><Link to="/about">About me</Link></li>
+                            <li className="menu-item menu-item-type-post_type menu-item-object-page"><a href="https://www.blog.rgllm.com/">Writing</a></li>
 				        </ul>
                     </div>
-			        <div class="header-right">
-                        <div class="social">
-                            <a href="https://www.twitter.com/rgllm" class="twitter"><span>Twitter</span></a>
-                            <a href="https://github.com/rgllm" class="github"><span>Github</span></a>
-                            <a href="https://www.instagram.com/rgllm" class="instagram"><span>Instagram</span></a>
-                            <a href="mailto:r@rgllm.com" class="email"><span>Email</span></a>
+			        <div className="header-right">
+                        <div className="social">
+                            <a href="https://www.twitter.com/rgllm" className="twitter"><span>Twitter</span></a>
+                            <a href="https://github.com/rgllm" className="github"><span>Github</span></a>
+                            <a href="https://www.instagram.com/rgllm" className="instagram"><span>Instagram</span></a>
+                            <a href="mailto:r@rgllm.com" className="email"><span>Email</span></a>
                         </div>
 			        </div>
                 </header>
