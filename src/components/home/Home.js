371dcd57d923c20aec674655cd732b0440edba39
Rogério Moreira <r@rgllm.com>
2018-09-29 01:33:34 +0100

    added pixel

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,7 +13,7 @@ class Home extends React.Component {
                 <main class="site-content">
                     <section class="intro-title screen-height header-compensated center-vertically">
                         <div class="section-inner fade-block">
-                            <h1 class="big-title graphics-title">Hi! My name is <span class="rgllm">Rogério</span>, and I’m a software engineer living in <span class="braga">Braga</span>. Currently taking my MSc. at <a href="https://www.uminho.pt" class="company">UMinho.</a></h1>
+                            <h1 class="big-title graphics-title">Hi! My name is <span class="rgllm">Rogério</span>, and I’m a software engineer living in <span class="braga">Braga</span>. Front-End Developer at <a href="https://www.pixelmatters.com" class="company">Pixelmatters.</a> and taking my MSc. at UMinho.</h1>
                         </div>
                     </section>
                 </main>
