f3331d6b1916f610f0f9f75a72f540a769008c20
Rogério Moreira <rgllm@rgllm.lan>
2018-10-13 17:53:21 +0100

    correcting https

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,7 +8,7 @@ class Home extends React.Component {
                 <main class="site-content">
                     <section class="intro-title screen-height header-compensated center-vertically">
                         <div class="section-inner fade-block">
-                            <h1 class="big-title graphics-title">Hi! My name is <span class="rgllm">Rogério</span> and I live in <span class="braga">Braga</span>. Currently taking my MSc. at UMinho and building things at <a href="https://www.pixelmatters.com" class="company">Pixelmatters</a>. </h1>
+                            <h1 class="big-title graphics-title">Hi! My name is <span class="rgllm">Rogério</span> and I live in <span class="braga">Braga</span>. Currently taking my MSc. at UMinho and building things at <a href="http://www.pixelmatters.com" class="company">Pixelmatters</a>. </h1>
                         </div>
                     </section>
                 </main>
