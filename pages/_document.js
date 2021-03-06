d2f7e945ddbc3651867df6d131017939e098158f
Rogério Moreira <r@rgllm.com>
2021-03-06 21:14:24 +0000

    remove next-google-font default export in favor of named

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import GoogleFonts from 'next-google-fonts';
+import { GoogleFonts } from 'next-google-fonts';
 class MyDocument extends Document {
   static getInitialProps(ctx) {
     return Document.getInitialProps(ctx)
@@ -20,8 +20,8 @@ class MyDocument extends Document {
           />
           <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
           <link href="/static/favicons/manifest.json" rel="manifest" />
-          <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin=""/>
-          <link rel="preconnect" href="https://cdn.splitbee.io/" crossOrigin=""/>
+          <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="" />
+          <link rel="preconnect" href="https://cdn.splitbee.io/" crossOrigin="" />
           <script async src="https://cdn.splitbee.io/sb.js"></script>
         </Head>
         <body>
