64f47054db26c2ff6c96993a22dfcdd890619728
Rogério Moreira <r@rgllm.com>
2021-07-01 16:24:57 +0100

    add se security headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,39 @@
+// next.config.js
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  }					
+];
+
 module.exports = {
-  future: {
-    webpack5: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
   },
 }
