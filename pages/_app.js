e6cb2e6894e255a12cd29b51c44d4f363db21775
Rogério Moreira <r@rgllm.com>
2021-09-15 15:26:45 +0100

    add kbar actions

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,37 @@ import { CSSReset, ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
 import { DefaultSeo } from 'next-seo';
 import { Global, css } from '@emotion/react';
 import Head from 'next/head';
+import { KBarProvider } from 'kbar';
 
 import '@/styles/components/PostContent.scss';
 import SEO from '../next-seo.config';
 import theme from '@/styles/theme';
 
 const App = ({ Component, pageProps }) => {
+  const kbarActions = [
+    {
+      id: 'blog',
+      name: 'Blog',
+      shortcut: ['b'],
+      keywords: 'blog',
+      perform: () => window.location.pathname = 'blog'
+    },
+    {
+      id: 'about',
+      name: 'about',
+      shortcut: ['r'],
+      keywords: 'about',
+      perform: () => window.location.pathname = 'about'
+    }
+  ]
+	
   return (
-    <ChakraProvider theme={theme}>
-      <ColorModeProvider options={{ useSystemColorMode: true,  initialColorMode: 'light' }}>
-        <CSSReset />
-        <Global
-          styles={css`
+    <KBarProvider actions={kbarActions}>
+      <ChakraProvider theme={theme}>
+        <ColorModeProvider options={{ useSystemColorMode: true,  initialColorMode: 'light' }}>
+          <CSSReset />
+          <Global
+            styles={css`
             html {
               scroll-behavior: smooth;
             }
@@ -26,14 +45,15 @@ const App = ({ Component, pageProps }) => {
 							background: black;
 						}
           `}
-        />
-        <Head>
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
-        </Head>
-        <DefaultSeo {...SEO} />
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+          />
+          <Head>
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+          </Head>
+          <DefaultSeo {...SEO} />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </KBarProvider>
   )
 }
 
