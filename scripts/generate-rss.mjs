3d4795d7c20b419125d237db722834529e60fee7
Rogério Moreira <r@rgllm.com>
2022-04-10 18:33:28 +0100

    add post build command

--- scripts/generate-rss.mjs
+++ scripts/generate-rss.mjs
@@ -1,10 +1,82 @@
 import { writeFileSync } from 'fs'
 import RSS from 'rss'
+import axios from 'axios'
+import slugify from 'slugify'
 
-import { getAllPosts } from '/lib/get-posts.ts'
+function toJson(data) {
+	if (data !== undefined) {
+		let intCount = 0,
+			repCount = 0
+		const json = JSON.stringify(data, (_, v) => {
+			if (typeof v === 'bigint') {
+				intCount++
+				return `${v}#bigint`
+			}
+			return v
+		})
+		const res = json.replace(/"(-?\d+)#bigint"/g, (_, a) => {
+			repCount++
+			return a
+		})
+		if (repCount > intCount) {
+			// You have a string somewhere that looks like "123#bigint";
+			throw new Error(`BigInt serialization conflict with a string value.`)
+		}
+		return res
+	}
+}
+
+function addExtraDataToPost(post) {
+	const { id, bodyHTML, title, publishedAt, body, bodyText } = post
+
+	return {
+		id,
+		date: publishedAt,
+		title,
+		body,
+		bodyHTML,
+		slug: slugify(title, { lower: true }),
+		description: bodyText.split('.')[0] + '.',
+	}
+}
+
+async function getPosts() {
+	const result = await axios({
+		url: 'https://api.github.com/graphql',
+		method: 'POST',
+		headers: {
+			Authorization: `bearer ${process.env.GH_TOKEN}`,
+		},
+		data: {
+			query: `
+        query {
+          repository(owner: "rgllm", name: "rgllm.com") {
+            discussions(first:100, categoryId: "DIC_kwDOAtQYB84COZvx") {
+              nodes {
+                id
+                body
+                bodyText
+                bodyHTML
+                url
+                updatedAt
+                title
+                publishedAt
+                category {
+                  name
+                }
+            }
+          }
+        }
+        }`,
+		},
+	})
+
+	const allPosts = JSON.parse(toJson(result.data.data.repository.discussions.nodes))
+	return allPosts.map((post) => addExtraDataToPost(post))
+}
 
 async function generate() {
-	const posts = await getAllPosts()
+	const posts = await getPosts()
 
 	const blogFeed = new RSS({
 		title: 'Rogério Moreira',
@@ -12,8 +84,8 @@ async function generate() {
 		feed_url: 'https://rgllm.com/feed.xml',
 	})
 
-	allBlogs.map((post) => {
-		feed.item({
+	posts.map((post) => {
+		blogFeed.item({
 			title: post.title,
 			url: `https://rgllm.com/blog/${post.slug}`,
 			date: post.date,
@@ -21,7 +93,7 @@ async function generate() {
 		})
 	})
 
-	writeFileSync('./public/feed.xml', feed.xml({ indent: true }))
+	writeFileSync('./public/feed.xml', blogFeed.xml({ indent: true }))
 }
 
 generate()
--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -1,10 +1,85 @@
 import { writeFileSync } from 'fs'
 import { globby } from 'globby'
 import prettier from 'prettier'
+import axios from 'axios'
+import slugify from 'slugify'
+
+function toJson(data) {
+	if (data !== undefined) {
+		let intCount = 0,
+			repCount = 0
+		const json = JSON.stringify(data, (_, v) => {
+			if (typeof v === 'bigint') {
+				intCount++
+				return `${v}#bigint`
+			}
+			return v
+		})
+		const res = json.replace(/"(-?\d+)#bigint"/g, (_, a) => {
+			repCount++
+			return a
+		})
+		if (repCount > intCount) {
+			// You have a string somewhere that looks like "123#bigint";
+			throw new Error(`BigInt serialization conflict with a string value.`)
+		}
+		return res
+	}
+}
+
+function addExtraDataToPost(post) {
+	const { id, bodyHTML, title, publishedAt, body, bodyText } = post
+
+	return {
+		id,
+		date: publishedAt,
+		title,
+		body,
+		bodyHTML,
+		slug: slugify(title, { lower: true }),
+		description: bodyText.split('.')[0] + '.',
+	}
+}
+
+async function getPosts() {
+	const result = await axios({
+		url: 'https://api.github.com/graphql',
+		method: 'POST',
+		headers: {
+			Authorization: `bearer ${process.env.GH_TOKEN}`,
+		},
+		data: {
+			query: `
+        query {
+          repository(owner: "rgllm", name: "rgllm.com") {
+            discussions(first:100, categoryId: "DIC_kwDOAtQYB84COZvx") {
+              nodes {
+                id
+                body
+                bodyText
+                bodyHTML
+                url
+                updatedAt
+                title
+                publishedAt
+                category {
+                  name
+                }
+            }
+          }
+        }
+        }`,
+		},
+	})
+
+	const allPosts = JSON.parse(toJson(result.data.data.repository.discussions.nodes))
+	return allPosts.map((post) => addExtraDataToPost(post))
+}
 
 async function generate() {
 	const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
 	const pages = await globby(['pages/*.tsx', '!pages/_*.tsx', '!pages/api', '!pages/404.tsx'])
+	const posts = await getPosts()
 
 	const sitemap = `
     <?xml version="1.0" encoding="UTF-8"?>
@@ -21,6 +96,15 @@ async function generate() {
             `
 					})
 					.join('')}
+					${posts
+						.map((post) => {
+							return `
+              <url>
+                  <loc>${`https://rgllm.com/blog/${post.slug}`}</loc>
+              </url>
+            `
+						})
+						.join('')}
     </urlset>
     `
 
