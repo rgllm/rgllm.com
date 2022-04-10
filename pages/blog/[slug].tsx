626f158fb32f48845e12c7101f1e15f3381d5b26
Rogério Moreira <r@rgllm.com>
2022-04-10 21:48:21 +0100

    Various fixes

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -7,11 +7,11 @@ import convertToComponents from 'lib/parse-html'
 export default function Blog({ post }: InferGetStaticPropsType<typeof getStaticProps>) {
 	if (!post?.title) return <></>
 
-	const { title, bodyHTML } = post
+	const { title, bodyHTML, description, date } = post
 	const parsedBody = convertToComponents(bodyHTML)
 
 	return (
-		<Container>
+		<Container title={`${title} - Rogério Moreira`} description={`${description}`} date={date}>
 			<div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
 				<h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl">{title}</h1>
 				<div className="prose">{parsedBody}</div>
