0e89afc843b0af4f1aac36e1e7c31e7c8fb35383
Rogério Moreira <r@rgllm.com>
2022-08-06 20:44:12 +0100

    Use edge runtime

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -1,33 +1,37 @@
-import {NextApiRequest, NextApiResponse} from 'next'
-
-const axios = require('axios')
+import {type NextRequest} from 'next/server'
 
 const bearer_token = process.env.WEATHER_BEARER_TOKEN
 const api_url = process.env.WEATHER_API_URL
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const response = await await axios({
-      url: api_url,
-      method: 'GET',
+export default async function handler(req: NextRequest) {
+  const response = await fetch(api_url, {
+    method: req.method,
+    headers: {
+      Authorization: `Bearer ${bearer_token}`,
+      'Content-Type': 'application/json',
+    },
+  })
+
+  const result = await response.json()
+
+  return new Response(
+    JSON.stringify({
+      temperature: result?.attributes?.temperature,
+      pressure: result?.attributes?.pressure,
+      humidity: result?.attributes?.humidity,
+      unit_of_measurement: result?.attributes?.unit_of_measurement,
+      updated: result?.last_updated,
+    }),
+    {
+      status: 200,
       headers: {
-        Authorization: `Bearer ${bearer_token}`,
-        'Content-Type': 'application/json',
+        'content-type': 'application/json',
+        'cache-control': 'max-age=0, s-maxage=600',
       },
-    })
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(
-      JSON.stringify({
-        temperature: response?.data?.attributes?.temperature,
-        pressure: response?.data?.attributes?.pressure,
-        humidity: response?.data?.attributes?.humidity,
-        unit_of_measurement: response?.data?.attributes?.unit_of_measurement,
-        updated: response?.data?.last_updated,
-      }),
-    )
-  } catch (error) {
-    res.json(error)
-    res.status(500).end()
-  }
+    },
+  )
+}
+
+export const config = {
+  runtime: 'experimental-edge',
 }
