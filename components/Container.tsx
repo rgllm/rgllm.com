9492a680216c318ceddc12f67bf443508839d620
Rogério Moreira <r@rgllm.com>
2022-08-10 17:43:52 +0100

    Add RSS feed to head

--- components/Container.tsx
+++ components/Container.tsx
@@ -45,6 +45,7 @@ export default function Container(props) {
         <meta content={meta.description} name="description" />
         <meta property="og:url" content={`https://rgllm.com${router.asPath}`} />
         <link rel="canonical" href={`https://rgllm.com${router.asPath}`} />
+        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Rogério Moreira" />
         <meta property="og:description" content={meta.description} />
