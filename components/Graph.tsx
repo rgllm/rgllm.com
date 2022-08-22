554fe0ea16bf2e572e9b568d0f23ee76d59ce2c4
Rogério Moreira <r@rgllm.com>
2022-08-22 15:16:22 +0100

    Graphs

--- components/Graph.tsx
+++ components/Graph.tsx
@@ -1,4 +1,4 @@
-import {LineChart, Line, CartesianGrid, XAxis, YAxis} from 'recharts'
+import {LineChart, Legend, Line, CartesianGrid, XAxis, YAxis} from 'recharts'
 
 export default function Graph({data, ykey, xkey}) {
   return (
@@ -8,7 +8,8 @@ export default function Graph({data, ykey, xkey}) {
       data={data}
       margin={{top: 5, right: 20, bottom: 5, left: 0}}
     >
-      <Line type="monotone" dataKey={ykey} stroke="black" strokeWidth={1} dot={false} />
+      <Legend verticalAlign="top" height={36} />
+      <Line type="monotone" dataKey={ykey} stroke="black" strokeWidth={1.5} dot={false} />
       <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
       <XAxis dataKey={xkey} stroke="#ccc" strokeWidth={0.5} fontSize={0} />
       <YAxis stroke="#ccc" strokeWidth={0.5} />
