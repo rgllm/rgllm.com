bc9a856cf49f28dc068a74e59c6a7f6d340a8603
Rogério Moreira <r@rgllm.com>
2023-11-17 10:12:33 +0000

    Visual stuff

--- components/BookmarksList.tsx
+++ components/BookmarksList.tsx
@@ -6,8 +6,8 @@ import {truncateText} from 'lib/truncate'
 export default function BookmarksList({bookmarks}) {
   return (
     <ul role="list" className="w-full -my-5 divide-y divide-gray-200">
-      {bookmarks?.map(bookmark => (
-        <li key={bookmark.id} className="py-4">
+      {bookmarks?.map((bookmark, index) => (
+        <li key={index} className="py-4">
           <div className="flex items-center space-x-4">
             <div className="flex items-center justify-center">
               <Image
@@ -21,7 +21,7 @@ export default function BookmarksList({bookmarks}) {
             <div className="flex-1 min-w-0">
               <p className="font-medium text-gray-900 truncate">{bookmark.title}</p>
               <p className="text-gray-500 max-w-[500px] text-sm">
-                {truncateText(bookmark.description, 300)}
+                {truncateText(bookmark.description, 280)}
               </p>
             </div>
             <div>
