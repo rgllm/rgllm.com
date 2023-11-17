e183ada4b94999bf498a774d3bcc15c1d0828f76
Rogério Moreira <r@rgllm.com>
2023-11-17 10:06:27 +0000

    SEO stuff

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function PostCard({title, slug}) {
   return (
     <Link href={`/blog/${slug}`}>
-      <h4 className="w-full mb-4 font-medium text-gray-600 text-md">{title}</h4>
+      <h3 className="w-full mb-4 font-medium text-gray-600 text-md">{title}</h3>
     </Link>
   )
 }
