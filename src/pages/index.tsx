7d2bcc32765915743bd322226636075c168d2de1
Rogério Moreira <r@rgllm.com>
2025-06-04 09:59:34 +0100

    Fix page import and clean shell artifacts (#1731)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { generateHtml } from '../utils/generateHtml'
 import { htmlResponse } from '../utils/response'
 
 export default async function index() {
-	const html = await generateHtml(IndexPage)
-	return htmlResponse(html)
+        const html = await generateHtml(IndexPage)
+        return htmlResponse(html)
 }
+
