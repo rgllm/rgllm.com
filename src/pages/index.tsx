df625f745bc9d7b7701db8625b69514d62813008
Rogério Moreira <r@rgllm.com>
2025-06-04 10:18:49 +0100

    refactor: remove async from HTML generator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { generateHtml } from '../utils/generateHtml'
 import { htmlResponse } from '../utils/response'
 
 export default async function index() {
-        const html = await generateHtml(IndexPage)
+        const html = generateHtml(IndexPage)
         return htmlResponse(html)
 }
 
